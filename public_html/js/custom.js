07566f9faee78d90c27cef40bbd6c6c7d72d5a24
Alexandra <aeslinares@gmail.com>
2017-02-28 00:10:04 -0500

    changing images and join us page

--- public_html/js/custom.js
+++ public_html/js/custom.js
@@ -1,29 +1,51 @@
 $(document).ready(function(){
-    $('.center').slick({
-      centerPadding: '30px',
-      slidesToShow: 7,
-      autoplay: true,
-      autoplaySpeed: 750,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '10px',
-            slidesToShow: 5
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            centerPadding: '10px',
-            slidesToShow: 3
-          }
-        }
-      ]
-    });
+  var phrases = [
+  "Valor que impulsa tu crecimiento",
+  "Innovación que potencia tus beneficios",
+  "Soluciones que atienden tus necesidades",
+  "Tecnologías que agilizan tus procesos y servicios",
+  "Cercanía que genera tu confianza"
+  ];
 
+  $('.center').slick({
+    centerPadding: '30px',
+    slidesToShow: 7,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '10px',
+          slidesToShow: 5
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '10px',
+          slidesToShow: 3
+        }
+      }
+    ]
   });
+
+  var $carousel = $('#carouselHeader');
+  $carousel.carousel();
+
+  var active = $carousel.find('li.active');
+  var index = active.data('slide-to');
+  $('#phrase').text(phrases[index]);
+
+  $carousel.bind('slide.bs.carousel', function (e) {
+    active = $carousel.find('li.active');
+    index = ( active.data('slide-to') + 1 ) % 5;
+
+    $('#phrase').text(phrases[index]);
+  });
+
+});
