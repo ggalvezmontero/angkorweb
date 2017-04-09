a97142f72675ead200da09271a1194f9d0309498
Bryam <bryam.pacco@gmail.com>
2017-04-09 01:21:59 -0500

    unete mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,6 +1,6 @@
 class NotificationMailer < ApplicationMailer
   require 'open-uri'
-  default to: 'bryam.pacco@gmail.com'
+  default to: 'aespichan@angkorperu.com'
 
   def contact(params)
     @contact = params
@@ -9,12 +9,10 @@ class NotificationMailer < ApplicationMailer
 
   def join_us(params)
   	@contact = params
-    p "hola"
     cv = @contact.cv.url
     cv = "https://s3.amazonaws.com/dreamstoreme-staging/dreams/main_images/000/000/012/original/banner-01.jpg?1451774055"
     cv_file = open(cv)
     cv_file_path = cv_file.path
-    p cv_file_path
   	attachments['cv.pdf'] = File.read(cv_file_path)
   	mail from: @contact.email, subject: 'Un usuario quiere unirse'
   end
