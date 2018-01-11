f5f884324d7e5f8e6a53708d30c97ce751dd2d46
Alexandra <aeslinares@gmail.com>
2018-01-11 16:19:03 -0500

    ädd images and redirect to pecave

---
+++ app/controllers/o7invoice_controller.rb
@@ -0,0 +1,14 @@
+clientes = { "pecave" => "http://181.65.247.205/PECAVE"}
+
+
+class o7invoiceController < ApplicationController
+	def shows
+		redirect_to clientes[cliente]
+	end
+
+	private
+
+	def cliente
+		params.permit(:cliente)[":cliente"]
+	end
+end
