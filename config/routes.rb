0f3bbe1b6dcc0cadd2fbcca5947f51a8734f5146
Alexandra <aeslinares@gmail.com>
2018-05-03 18:56:17 -0500

    exterran

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   get 'lopezingenieros' => 'o7invoice#lopezingenieros'
   get 'lavanderiafashion' => 'o7invoice#lavanderiafashion'
   get 'EXTERRAN' => 'o7invoice#exterran'
+  get 'exterran' => 'o7invoice#exterran'
 end
